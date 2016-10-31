edb2a764e206b417f8b5f615c1c36ca4fb4c143b
Grzegorz Slowikowski <gslowikowski@gmail.com>
2016-10-31 14:10:42 +0100

    Don't use deprecated classes and methods.
    Deprecated classes:
    - play.api.test.FakeApplication.
    
    Deprecated methods:
    - play.api.test.RouteInvokers.route[T](req: Request[T])(implicit w: Writeable[T]): Option[Future[Result]].

--- playframework/multimodule/main/test/ApplicationSpec.scala
+++ playframework/multimodule/main/test/ApplicationSpec.scala
@@ -14,20 +14,17 @@ class ApplicationSpec extends SpecificationWithJUnit {
   
   "Application" should {
     
-    "send 404 on a bad request" in {
-      running(FakeApplication()) {
-        route(FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
-      }
+    "send 404 on a bad request" in new WithApplication {
+      route(app, FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
     }
     
-    "render the index page" in {
-      running(FakeApplication()) {
-        val home = route(FakeRequest(GET, "/")).get
+    "render the index page" in new WithApplication {
+      val home = route(app, FakeRequest(GET, "/")).get
         
-        status(home) must equalTo(OK)
-        contentType(home) must beSome.which(_ == "text/html")
-        contentAsString(home) must contain ("Your new application is ready.")
-      }
+      status(home) must equalTo(OK)
+      contentType(home) must beSome.which(_ == "text/html")
+      contentAsString(home) must contain ("Your new application is ready.")
     }
+
   }
 }
