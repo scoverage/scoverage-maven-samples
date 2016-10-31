19a505a03e3421e630d79505337d2719fce6d60c
Grzegorz Slowikowski <gslowikowski@gmail.com>
2016-10-31 13:49:19 +0100

    Don't use deprecated classes and methods.
    Deprecated classes:
    - play.api.test.FakeApplication.
    
    Deprecated methods:
    - play.api.test.RouteInvokers.route[T](req: Request[T])(implicit w: Writeable[T]): Option[Future[Result]].

--- playframework/singlemodule/helloworld/helloworld-scala-2.11/test/ApplicationSpec.scala
+++ playframework/singlemodule/helloworld/helloworld-scala-2.11/test/ApplicationSpec.scala
@@ -10,34 +10,30 @@ class ApplicationSpec extends Specification {
   
   "Application" should {
     
-    "send 404 on a bad request" in {
-      running(FakeApplication()) {
-        //route(FakeRequest(GET, "/boum")) must beNone        
-        route(FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
-      }
+    "send 404 on a bad request" in new WithApplication {
+      route(app, FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
     }
     
-    "render an empty form on index" in {
-      running(FakeApplication()) {
-        val home = route(FakeRequest(GET, "/")).get
+    "render an empty form on index" in new WithApplication {
+      val home = route(app, FakeRequest(GET, "/")).get
         
-        status(home) must equalTo(OK)
-        contentType(home) must beSome.which(_ == "text/html")
-      }
+      status(home) must equalTo(OK)
+      contentType(home) must beSome.which(_ == "text/html")
     }
-      "send BadRequest on form error" in {
-      running(FakeApplication()) {
-        val home = route(FakeRequest(GET, "/hello?name=Bob&repeat=xx")).get
-        status(home) must equalTo(BAD_REQUEST)
-        contentType(home) must beSome.which(_ == "text/html")
-      }
+
+    "send BadRequest on form error" in new WithApplication {
+      val home = route(app, FakeRequest(GET, "/hello?name=Bob&repeat=xx")).get
+
+      status(home) must equalTo(BAD_REQUEST)
+      contentType(home) must beSome.which(_ == "text/html")
     }
-    "say hello" in {
-      running(FakeApplication()) {
-        val home = route(FakeRequest(GET, "/hello?name=Bob&repeat=10")).get
-        status(home) must equalTo(OK)
-        contentType(home) must beSome.which(_ == "text/html")
-      }
+
+    "say hello" in new WithApplication {
+      val home = route(app, FakeRequest(GET, "/hello?name=Bob&repeat=10")).get
+
+      status(home) must equalTo(OK)
+      contentType(home) must beSome.which(_ == "text/html")
     }
+
   } 
 }
